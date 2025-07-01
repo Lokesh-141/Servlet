033734482bfd25d07a7c8d9f64f388878f27a5e8
Lokesh-141 <166227979+Lokesh-141@users.noreply.github.com>
2025-07-01 12:35:02 +0530

    Create RegisterServlet.java

---
+++ TechBlog/Source Packages/com.tech.blog.servlets/RegisterServlet.java
@@ -0,0 +1,44 @@
+package com.tech.blog.servlets;
+
+import com.tech.blog.dao.UserDao;
+import com.tech.blog.entities.User;
+import com.tech.blog.helper.ConnectionProvider;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+@MultipartConfig
+public class RegisterServlet extends HttpServlet {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            String check = request.getParameter("check");
+            if (check == null) {
+                out.println("box not checked");
+            } else {
+                String name = request.getParameter("user_name");
+                String email = request.getParameter("user_email");
+                String password = request.getParameter("user_password");
+                String gender = request.getParameter("gender");
+                String about = request.getParameter("about");
+                User user = new User(name, email, password, gender, about);
+                
+                UserDao dao = new UserDao(ConnectionProvider.getConnection());
+                if (dao.saveUser(user)) {
+                    out.println("done");
+                } else {
+                    out.println("error");
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+}
