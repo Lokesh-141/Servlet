5e755d00671f55535c7c050af168634975490200
Lokesh-141 <166227979+Lokesh-141@users.noreply.github.com>
2025-07-01 12:34:02 +0530

    Create LoginServlet.java

---
+++ TechBlog/Source Packages/com.tech.blog.servlets/LoginServlet.java
@@ -0,0 +1,43 @@
+package com.tech.blog.servlets;
+
+import com.tech.blog.dao.UserDao;
+import com.tech.blog.entities.Message;
+import com.tech.blog.entities.User;
+import com.tech.blog.helper.ConnectionProvider;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class LoginServlet extends HttpServlet {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            String userEmail = request.getParameter("email");
+            String userPassword = request.getParameter("password");
+            UserDao dao = new UserDao(ConnectionProvider.getConnection());
+            User u = dao.getUserByEmailAndPassword(userEmail, userPassword);
+            
+            if (u == null) {
+//                out.println("Invalid Details..try again");
+                Message msg = new Message("Invalid Details ! try with another","error","alert-danger");
+                HttpSession s = request.getSession();
+                s.setAttribute("msg", msg);
+                response.sendRedirect("login_page.jsp");
+            } else {
+                HttpSession s = request.getSession();
+                s.setAttribute("currentUser", u);
+                response.sendRedirect("profile.jsp");
+            }
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }    
+}
